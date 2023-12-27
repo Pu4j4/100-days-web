11c3704217a39e9c7051cb513776e7a5ad3373ed
bhanu <bhanubethala2@gmail.com>
2023-12-27 23:13:33 +0530

    Day - 1
    program 1,2

---
+++ js/1program.js
@@ -0,0 +1,6 @@
+const stu_details {
+    stuname : "Bhanu",
+    phno : "8247623492",
+    marks : "85"
+};
+console.log(stu_details);
---
+++ js/2program.js
@@ -0,0 +1,34 @@
+let a = null;
+let b = 888;
+let c = true; //can also be false
+let d = bigint("149")
+let e = "ashish"
+let f = symbol("nice symbol")
+let g = underfined
+console.log(a, b, c, d, e, f, g)
+//variable of type string and add number to it
+let bhanu = 20;
+
+//use typeof to find datatype 
+console.log(typeof bhanu)
+
+//const object in javascript can you change it to hold a number later
+//no we can't we have to assign value at time of const declaration
+//const a; it is not possible
+
+//add key to const object
+const an {
+    animal : "lion"
+}
+
+
+//create word meaning dictionary 
+
+const dict{
+    Grateful : "feeling or showing an appreciation of kindness",
+    Genetic : "relating to genes or heredity",
+    Gigantic : "of very great size",
+    Gaze : "look steadily and intently"
+}
+console.log(Grateful)
+
