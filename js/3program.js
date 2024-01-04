736af5274ccf313113af4638b6ecd7626cd79735
bhanu <bhanubethala2@gmail.com>
2024-01-04 22:30:41 +0530

    Day-6
    alert,prompt,confirm

---
+++ js/3program.js
@@ -0,0 +1,14 @@
+//alert
+alert("Hello");
+
+//prompt
+result = prompt(title, [default]);
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); 
+
+//confirm
+result = confirm(question);
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss )
