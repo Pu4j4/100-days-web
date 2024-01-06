b55ab07d7f7bbfb865ebc3cd46be7b98b31f394f
bhanu <bhanubethala2@gmail.com>
2024-01-06 19:04:06 +0530

    Day-8
    scores of tiger and cheetah

---
+++ js/tigerandcheetah.js
@@ -0,0 +1,10 @@
+const tiger = (32+20+29+49)/4
+const cheetah = (30+49+19+2)/4
+if(tiger>cheetah){
+    console.log(`tiger scored ${tiger} than cheetah`);
+}
+else if (tiger<cheetah) {
+    console.log(`cheetah scored ${cheetah} than tiger`);
+} else {
+    console.log("both are scored same")
+}
