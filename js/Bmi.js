fb7b8cab419457060ac665c4ca4e72d68cbff7b5
bhanu <bhanubethala2@gmail.com>
2024-01-06 18:38:06 +0530

    Day-8
    Body mass index

---
+++ js/Bmi.js
@@ -0,0 +1,7 @@
+const bhanu_mass = 23;
+const bhanu_height = 1.6;
+const sweety_mass = 20;
+const sweety_height = 1.2;
+const BMI_bhanu = bhanu_mass / (bhanu_height * bhanu_height);
+const BMI_sweety = sweety_mass / (sweety_height * sweety_height);
+console.log(BMI_bhanu,BMI_sweety);
