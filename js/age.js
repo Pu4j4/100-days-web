bda8c60b89eb8adcf30ec4b0c294964a450d360b
bhanu <bhanubethala2@gmail.com>
2024-01-06 19:22:12 +0530

    Day-8
    age

---
+++ js/age.js
@@ -0,0 +1,15 @@
+const age = 21;
+age>=20 ? console.log("you can drive") :
+console.log("you cannot drive");
+
+const drink = age>=21 ? "you can drink" : "you cannot drink";
+console.log(drink)
+
+
+let drink1;
+if(age>=18){
+    drink1 = "wine"
+}else{
+    drink1 = "water"
+}
+console.log(drink1)
