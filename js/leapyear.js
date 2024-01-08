6aa2fc4e6b17dbf407230595193022cf3c7533e9
bhanu <bhanubethala2@gmail.com>
2024-01-08 23:25:41 +0530

    Day-10
    leap year

---
+++ js/leapyear.js
@@ -0,0 +1,9 @@
+const leapyear = (year) => {
+    return (year % 100 == 0) ? (year % 400 == 0) : (year % 4 == 0);
+ };
+ console.log(leapyear(2016));
+ console.log(leapyear(2000));
+ console.log(leapyear(1700));
+ console.log(leapyear(1800));
+ console.log(leapyear(2004));
+ 
