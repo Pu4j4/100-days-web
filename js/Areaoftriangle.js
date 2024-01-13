0744d2d4f6d16ac55260d4027bf8f92248911e95
bhanu <bhanubethala2@gmail.com>
2024-01-13 23:05:25 +0530

    Day-15
    area of triangle

---
+++ js/Areaoftriangle.js
@@ -0,0 +1,15 @@
+const side1 = parseInt(prompt('Enter side1: '));
+const side2 = parseInt(prompt('Enter side2: '));
+const side3 = parseInt(prompt('Enter side3: '));
+
+// calculate the semi-perimeter
+const s = (side1 + side2 + side3) / 2;
+
+//calculate the area
+const areaValue = Math.sqrt(
+  s * (s - side1) * (s - side2) * (s - side3)
+);
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
