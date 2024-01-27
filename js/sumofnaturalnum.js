d1b4e919fa62c093d21ee5f9c8f14a1b4c225aad
bhanu <bhanubethala2@gmail.com>
2024-01-27 23:03:38 +0530

    Day-23
    Sum of  natural numbers

---
+++ js/sumofnaturalnum.js
@@ -0,0 +1,14 @@
+// program to display the sum of natural numbers
+
+// take input from the user
+const number = parseInt(prompt('Enter a positive integer: '));
+
+let sum = 0;
+
+// looping from i = 1 to number
+// in each iteration, i is increased by 1
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+
+console.log('The sum of natural numbers:', sum);
